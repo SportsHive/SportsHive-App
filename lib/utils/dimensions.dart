d8275a22c973fbbf7d6e3e8fe7a4e17cff36a075
MBdeir <moey.bdeir@gmail.com>
2023-04-18 00:22:43 +0300

    Insert Events Page to Navigation environment
    Still needs to UI tweaks

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sportshive/screens/auth/add_post_screen.dart';
 import 'package:sportshive/screens/auth/create.dart';
+import 'package:sportshive/screens/auth/event_page.dart';
+
 
 import '../screens/auth/profile_screen.dart';
 
@@ -8,7 +10,7 @@ const webScreenSize = 600;
 
 
 const homeScreenItems = [
-  Text("Events"),
+  EventsScreen(),
   Text("Home"),
   CreatePost(),
   ProfileScreen(),
