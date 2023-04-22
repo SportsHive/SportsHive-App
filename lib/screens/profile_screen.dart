7ed16f86e09d700200a6bf262625813e6a3bb901
mfawaz24 <fawazmehdi@outlook.com>
2023-04-22 19:46:00 +0300

    make profile_screen.dart reusable, so it can be used for the current user's profile or to view another user's profile. link profile_screen and editprofile_page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,20 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sportshive/data/controllers/profile_controller.dart';
 import 'package:sportshive/data/repositories/user_repo.dart';
+import 'package:sportshive/screens/editprofile_page.dart';
 import 'package:sportshive/utils/colors.dart';
 
 import '../../data/models/user_model.dart';
 import '../../widgets/custom_option.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({
-    Key? key,
-  }) : super(key: key);
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  ProfileScreenState createState() => ProfileScreenState();
+}
+
+class ProfileScreenState extends State<ProfileScreen> {
+  static bool profileIsForCurrentUser() {
+    /**
+     * change when search is implemented to have different
+     * behavior for a different user on the profile screen
+     */
+    return true;
+  }
+
+  static bool isFollowing() {
+    /**
+     * change to check database and see if
+     * the person navigating follows the person
+     * who's profile is being displayed
+     */
+    return false;
+  }
+
+  static void updateFollow() {
+    /**
+     * change to check database and see if
+     * the person navigating follows the person
+     * who's profile is being displayed
+     */
+    if (isFollowing()) {
+      return;
+    } else {
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final userRepo = Get.put(UserRepository());
     userRepo.initState();
+
+    var followButtonText = '';
+    if (profileIsForCurrentUser()) {
+      followButtonText = "Edit profile";
+    } else if (isFollowing()) {
+      followButtonText = "Unfollow";
+    } else {
+      followButtonText = "Follow";
+    }
 
     return Scaffold(
       body: Padding(
@@ -104,14 +147,30 @@ class ProfileScreen extends StatelessWidget {
                               Container(
                                 width: double.infinity,
                                 child: ElevatedButton(
-                                  onPressed: () {},
-                                  child: Text('Follow'),
+                                  onPressed: () {
+                                    if (!profileIsForCurrentUser()) {
+                                      updateFollow();
+                                      setState(() {
+                                        followButtonText = isFollowing()
+                                            ? 'Unfollow'
+                                            : 'Follow';
+                                      });
+                                    } else {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                UserProfileScreen()),
+                                      );
+                                    }
+                                  },
                                   style: ElevatedButton.styleFrom(
                                     primary: Colors
                                         .orange, // Set the background color
                                     onPrimary:
                                         Colors.white, // Set the text color
                                   ),
+                                  child: Text(followButtonText),
                                 ),
                               ),
                             ],
