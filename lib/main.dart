b8c2beff9e35c8e62062a048f826d6954c620d6c
MBdeir <moey.bdeir@gmail.com>
2023-04-24 14:24:42 +0300

    Added Create Post Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sportshive/data/repositories/auth_repo.dart';
 import 'package:sportshive/responsive/mobile_screen_layout.dart';
 import 'package:sportshive/screens/create_events_screen.dart';
@@ -20,6 +21,7 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
