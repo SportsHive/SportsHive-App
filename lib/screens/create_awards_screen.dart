46b58f9cb09b007a143214fd49e57086f60da05e
MBdeir <moey.bdeir@gmail.com>
2023-04-28 03:45:42 +0300

    Updated Awards to be removable

--- lib/screens/create_awards_screen.dart
+++ lib/screens/create_awards_screen.dart
@@ -7,8 +7,9 @@ typedef OnAwardCreated = void Function(Award award);
 
 class CreateAwardsPage extends StatefulWidget {
   final OnAwardCreated onAwardCreated;
+  final Award? initialAward;
 
-  const CreateAwardsPage({Key? key, required this.onAwardCreated})
+  const CreateAwardsPage({Key? key, required this.onAwardCreated, this.initialAward})
       : super(key: key);
 
   @override
@@ -40,6 +41,15 @@ class _CreateAwardsPageState extends State<CreateAwardsPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    if (widget.initialAward != null) {
+      selectedAward = widget.initialAward!.imagePath;
+      awardNameController.text = widget.initialAward!.title;
+    }
+  }
+
+  @override
   void dispose() {
     awardNameController.dispose();
     super.dispose();
@@ -47,74 +57,91 @@ class _CreateAwardsPageState extends State<CreateAwardsPage> {
 
     @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: orange,
-        title: Text('Create Award'),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: awardNameController,
-              decoration: InputDecoration(
-                labelText: 'What is the name of your award?',
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: orange,
+      title: Text(widget.initialAward == null ? 'Create Award' : 'Edit Award'),
+    ),
+    body: SingleChildScrollView(
+      padding: const EdgeInsets.all(70.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: awardNameController,
+            decoration: InputDecoration(
+              labelText: 'What is the name of your award?',
+            ),
+          ),
+          SizedBox(height: 30.0),
+          Text('What icon best describes your award?'),
+          SizedBox(height: 20.0),
+          Wrap(
+            spacing: 30.0,
+            runSpacing: 30.0,
+            children: List.generate(awardIcons.length, (index) {
+              return GestureDetector(
+                onTap: () => selectAward(index),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  padding: EdgeInsets.all(
+                      selectedAward == awardIcons[index] ? 5.0 : 0.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                        selectedAward == awardIcons[index] ? 10.0 : 0.0),
+                    color: selectedAward == awardIcons[index]
+                        ? Colors.blue.withOpacity(0.5)
+                        : null,
+                  ),
+                  child: Image.asset(
+                    awardIcons[index],
+                    width: 60,
+                    height: 60,
+                  ),
+                ),
+              );
+            }),
+          ),
+          SizedBox(height: 16.0),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RoundedButton(
+                color: orange,
+                press: () {
+                  if (selectedAward != null &&
+                      awardNameController.text.isNotEmpty) {
+                    widget.onAwardCreated(Award(
+                      imagePath: selectedAward!,
+                      title: awardNameController.text,
+                    ));
+                  }
+                },
+                text: "Create",
               ),
             ),
-            SizedBox(height: 30.0),
-            Text('What icon best describes your award?'),
-            SizedBox(height: 20.0),
-            Wrap(
-              spacing: 20.0,
-              runSpacing: 20.0,
-              children: List.generate(awardIcons.length, (index) {
-                return GestureDetector(
-                  onTap: () => selectAward(index),
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    padding: EdgeInsets.all(
-                        selectedAward == awardIcons[index] ? 5.0 : 0.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                          selectedAward == awardIcons[index] ? 10.0 : 0.0),
-                      color: selectedAward == awardIcons[index]
-                          ? Colors.blue.withOpacity(0.5)
-                          : null,
-                    ),
-                    child: Image.asset(
-                      awardIcons[index],
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                );
-              }),
-            ),
-            SizedBox(height: 16.0),
+          ),
+          if (widget.initialAward != null)
             Align(
               alignment: Alignment.center,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: RoundedButton(
-                  color: orange,
+                  color: Colors.red,
                   press: () {
-                    if (selectedAward != null &&
-                        awardNameController.text.isNotEmpty) {
-                      widget.onAwardCreated(Award(
-                        imagePath: selectedAward!,
-                        title: awardNameController.text,
-                      ));
-                    }
+                    widget.onAwardCreated(Award(
+                      imagePath: '',
+                      title: '',
+                    ));
                   },
-                  text: "Create",
+                  text: "Remove",
                 ),
               ),
             ),
-          ],
-        ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
--- lib/widgets/award_display.dart
+++ lib/widgets/award_display.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sportshive/screens/create_awards_screen.dart';
-
-
 
 class Award {
   final String imagePath;
   final String title;
-  
 
   Award({required this.imagePath, required this.title});
 }
@@ -19,78 +17,155 @@ class AwardsWidget extends StatefulWidget {
 class _AwardsWidgetState extends State<AwardsWidget> {
   List<Award?> awards = List.generate(9, (index) => null);
 
-  void onAwardCreated(Award award, int index) {
+  void onAwardCreated(Award award, int index) async {
     setState(() {
       awards[index] = award;
     });
+
+    // Save awards to local storage
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> awardsList = awards
+        .map(
+            (award) => award != null ? '${award.imagePath}:${award.title}' : '')
+        .toList();
+    prefs.setStringList('awards', awardsList);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Load awards from local storage
+    loadAwards();
+  }
+
+  Future<void> loadAwards() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> awardsList =
+        prefs.getStringList('awards') ?? List.generate(9, (index) => '');
+    List<Award?> loadedAwards = awardsList.map((awardString) {
+      if (awardString.isEmpty) {
+        return null;
+      }
+      List<String> awardData = awardString.split(':');
+      return Award(imagePath: awardData[0], title: awardData[1]);
+    }).toList();
+    setState(() {
+      awards = loadedAwards;
+    });
+  }
+
+  Future<void> saveAwards() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> awardsList = awards
+        .map((award) =>
+            award != null ? '${award!.imagePath}:${award!.title}' : '')
+        .toList();
+    prefs.setStringList('awards', awardsList);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 0.0),
-      child: SingleChildScrollView(
-        child: Flexible(
-          child: GridView.count(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            crossAxisCount: 3,
-            childAspectRatio: 1.1,
-            mainAxisSpacing: 8,
-            crossAxisSpacing: 8,
-            children: List.generate(awards.length, (index) {
-              return InkWell(
-                onTap: () {
-                  if (awards[index] == null) {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (context) {
-                        return CreateAwardsPage(
-                          onAwardCreated: (newAward) {
-                            onAwardCreated(newAward, index);
-                            Navigator.pop(context);
-                          },
-                        );
-                      },
-                    );
-                  }
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: awards[index] == null ? Colors.grey : Colors.transparent,
-                      width: 2,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(
-                    child: awards[index] != null
-                        ? Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                awards[index]!.imagePath,
-                                height: 60,
-                                width: 60,
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 0.0),
+        child: SingleChildScrollView(
+          child: Flexible(
+            child: GridView.count(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              childAspectRatio: 1.1,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              children: List.generate(awards.length, (index) {
+                return InkWell(
+                  onTap: () {
+                    if (awards[index] == null) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return CreateAwardsPage(
+                            onAwardCreated: (newAward) {
+                              onAwardCreated(newAward, index);
+                              Navigator.pop(context);
+                            },
+                          );
+                        },
+                      );
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Remove Award'),
+                            content: Text(
+                                'Are you sure you want to remove this award?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Cancel'),
                               ),
-                              SizedBox(height: 4),
-                              Flexible(
-                                child: Text(
-                                  awards[index]!.title,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    awards[index] = null;
+                                  });
+                                  // Save awards to local storage
+                                  saveAwards();
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Remove'),
                               ),
                             ],
-                          )
-                        : Icon(Icons.add, size: 60, color: Colors.grey),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: awards[index] == null
+                            ? Colors.grey
+                            : Colors.transparent,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: awards[index] != null
+                          ? Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  awards[index]!.imagePath,
+                                  height: 60,
+                                  width: 60,
+                                ),
+                                SizedBox(height: 4),
+                                Flexible(
+                                  child: Text(
+                                    awards[index]!.title,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          : Icon(Icons.add, size: 60, color: Colors.grey),
+                    ),
                   ),
-                ),
-              );
-            }),
+                );
+              }),
+            ),
           ),
         ),
       ),
